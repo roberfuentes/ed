296e01f5e8277e561f25684206e16a05bf017e2f
roberfuentes <roberinstituto97@gmail.com>
2017-10-20 09:25:22 +0200

    Añadir contador a JAdivina

--- JAdivina/src/org/institutoserpis/ed/Adivina.java
+++ JAdivina/src/org/institutoserpis/ed/Adivina.java
@@ -7,26 +7,37 @@ public class Adivina {
 	public static void main(String[] args) {
 		Random random = new Random();
 		int numeroAleatorio = random.nextInt(5) +1;
-		
+		int contadorDeIntentos=1;
 		System.out.println("Numero aleatorio=" + numeroAleatorio);
 		Scanner number = new Scanner(System.in);
-		System.out.println("Adivina el numero entre (1 y 5: ");
+		System.out.printf("Adivina el numero entre (1 y 5:) [intento %s] : ",
+				contadorDeIntentos
+				);
 		
 		
 		String numberperson = number.nextLine();
 		int numberPersonInt = Integer.parseInt(numberperson);
 		
 		while(numberPersonInt != numeroAleatorio) {
+			contadorDeIntentos++;
 			if (numeroAleatorio > numberPersonInt) {
-				System.out.println("El numero introducido es menor al aleatorio. Vuelve a Intentarlo: ");
+				System.out.printf("El numero introducido es menor al aleatorio. Vuelve a Intentarlo. [intento%s] : ",
+				contadorDeIntentos
+				);
+				
 			}
 			else {
-				System.out.println("El numero introducido es mayor que el aleatorio. Vuelve a intentarlo " );
+				System.out.printf("El numero introducido es mayor que el aleatorio. Vuelve a intentarlo [intento%s] : ",
+						contadorDeIntentos
+						);
+				
 			}
 			numberperson = number.nextLine();
 			numberPersonInt = Integer.parseInt(numberperson);
 		}
-		System.out.println("El numero es correcto");
+		System.out.printf("El numero es correcto. Has acertado en %s %s.\n", 
+				contadorDeIntentos, contadorDeIntentos == 1 ? "intento" : "intentos"
+				);
 	number.close();
 	}
 
