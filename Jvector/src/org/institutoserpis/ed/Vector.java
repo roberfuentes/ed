769192453814a2a8c849f0185d87e0e16a32c05f
roberfuentes <roberinstituto97@gmail.com>
2017-10-27 09:15:16 +0200

    Añadir el Jvector restante

---
+++ Jvector/src/org/institutoserpis/ed/Vector.java
@@ -0,0 +1,15 @@
+package org.institutoserpis.ed;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		int[] v = new int[] {9, 15, 7, 12, 6};
+		
+		for (int index = 0; index<5; index++)
+				System.out.printf("v[%s]=%s\n", index, v[index]);
+		
+			
+
+	}
+
+}
