e1217491f9d31b31339d868ec5e709b7cc2e5d65
roberfuentes <roberinstituto97@gmail.com>
2017-11-14 08:24:55 +0100

    Modificado mas recommenders

--- JVector2/src/org/institutoserpis/ed/Vector2.java
+++ JVector2/src/org/institutoserpis/ed/Vector2.java
@@ -12,17 +12,42 @@ public class Vector2 {
 			
 			int value=15;
 			int position = indexOf(v, value);
-			System.out.println("position=" + position);
+			System.out.println("El mayor es " + position);
+			
+			int min = min(v);
+			System.out.println("min=" + min);
+			
 			
 			
 	}
 	
 	public static int indexOf(int [] v, int value){
-		int index=0;
-		while(index < v.length && v[index] != value)
-			index++;
-		if(index < v.length)
-			return index;
+		return 0;
+	}
+	public static int min(int[] v) {
+		return 77;
+	}
+	public static int indexOfMin(int[] v) {
 		return -1;
 	}
+	
+	public static void selectionSort(int[] v){
+		int selectedIndex = 0;
+		int indexOfMin = selectedIndex;
+		//for (int index = selectedIndex +1; index < v.length; index++) {	
+	}
+	
+	public static int sum(int[] v) {
+		int sum = 0;
+		//for (int index = 0; index < v.length; index++);
+			//sum = sum + v[index];
+		for (int item : v)
+			sum = sum + item;
+		return sum;
+	}
+	/*public static int min(int[] v) {
+		int posicionMin = 0;
+		for (int index = 1; index < v.length -1; index++)
+			if(v[index] < v[posicionMin] ) 
+				posicionMin = v[index];*/
 }
