ad3b95ec2e6357af61c8a05d604ddd284106524f
roberfuentes <roberinstituto97@gmail.com>
2018-01-09 08:09:49 +0100

    Aplicacion suma

---
+++ CSuma/CSuma/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		buttonSuma.Clicked += delegate {
+			int numeroCasilla1 = int.Parse(casilla1.Text);
+			int numeroCasilla2 = int.Parse(casilla2.Text);
+			int resultado = numeroCasilla1 + numeroCasilla2;
+			labelResultado.Text = resultado.ToString();
+
+		};
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
